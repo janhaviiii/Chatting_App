f373d8dc4024b2b34a3989a4e7838f3ac84afb08
Janhavi <janhavibawaskar028@gmail.com>
2021-07-04 01:33:23 +0530

    Sign In with firebase

---
+++ ChatApp/app/src/main/java/com/example/chatapp/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button signOutBtn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        signOutBtn=findViewById(R.id.signout_btn);
+        mAuth=FirebaseAuth.getInstance();
+
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(HomeActivity.this,SignInActivity.class ));
+                finish();
+
+            }
+        });
+    }
+}
